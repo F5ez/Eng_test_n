ed73c54d5933b4df5d02d0258b5fd6d3107cd0d1
F5ez <ksenchuk98@gmail.com>
2023-10-26 21:13:54 +0300

    bnt_f_size+v3.0

--- script.js
+++ script.js
@@ -5,121 +5,121 @@ const arr = [
     { questions: `1. Roberto is Italian. He’s ${clone.outerHTML} Italy` ,
         a:"from", b:"to", c:"at", answer:"from"
     },
-    { questions: `2.I leave for work ${clone.outerHTML} quarter past eight` ,
+    { questions: `2. I leave for work ${clone.outerHTML} quarter past eight` ,
         a:"in", b:"at", c:"on", answer:"at"
     },
-    { questions: `3.${clone.outerHTML} that man in the red hat?` ,
+    { questions: `3. ${clone.outerHTML} that man in the red hat?` ,
         a:"Whose", b:"Who", c:"Who's", answer:"Who's"
     },
-    { questions: `4.This letter is for Tom. Give it to ${clone.outerHTML}` ,
+    { questions: `4. This letter is for Tom. Give it to ${clone.outerHTML}` ,
         a:"him", b:"he", c:"his", answer:"him"
     },
-    { questions: `5.She ${clone.outerHTML} a letter at the moment.` ,
+    { questions: `5. She ${clone.outerHTML} a letter at the moment.` ,
         a:"write", b:"is writing", c:"writes", answer:"is writing"
     },
-    { questions: `6.What ${clone.outerHTML} on Saturdays?` ,
+    { questions: `6. What ${clone.outerHTML} on Saturdays?` ,
         a:"are you doing", b:"do you", c:"do you do", answer:"do you do"
     },
-    { questions: `7.Yesterday we ${clone.outerHTML} to the cinema and saw a great film`,
+    { questions: `7. Yesterday we ${clone.outerHTML} to the cinema and saw a great film`,
         a:"go", b:"have gone", c:"went", answer:"went"
     },
-    { questions: `8.${clone.outerHTML} your homework yet?`,
+    { questions: `8. ${clone.outerHTML} your homework yet?`,
         a:"Do you finish", b:"Have you finished", c:"Are you finishing", answer:"Have you finished"
     },
-    { questions: `9.It was a beautiful morning. The sun ${clone.outerHTML} and it was very warm. `,
+    { questions: `9. It was a beautiful morning. The sun ${clone.outerHTML} and it was very warm. `,
         a:"shines", b:"was shining", c:"shone", answer:"was shining"
     },
-    { questions: `10.“What ${clone.outerHTML} for a living?” “She’s a teacher.”.`,
+    { questions: `10. “What ${clone.outerHTML} for a living?” “She’s a teacher.”.`,
         a:"does she do", b:"is she doing", c:"does she", answer:"does she do"
     },
-    { questions: `11.George ${clone.outerHTML} to work hard but he does now.`,
+    { questions: `11. George ${clone.outerHTML} to work hard but he does now.`,
         a:"didn't", b:"used", c:"didn't use", answer:"used"
     },
-    { questions: `12.If she ${clone.outerHTML}, I’ll go on my own.`,
+    { questions: `12. If she ${clone.outerHTML}, I’ll go on my own.`,
         a:"comes", b:"doesn't come", c:"didn't come", answer:"doesn't come"
     },
-    { questions: `13.Mum asked me how long ${clone.outerHTML} for her.`,
+    { questions: `13. Mum asked me how long ${clone.outerHTML} for her.`,
         a:"I had been waiting", b:"had I been waiting", c:"have I been waiting", answer:"I had been waiting"
     },
-    { questions: `14.I think I ${clone.outerHTML} for you here. I just can’t go on walking!`,
+    { questions: `14. I think I ${clone.outerHTML} for you here. I just can’t go on walking!`,
         a:"will wait", b:"am going to wait", c:"am waiting", answer:"will wait"
     },
-    { questions: `15.Andrew has been painting ${clone.outerHTML} 2003.`,
+    { questions: `15. Andrew has been painting ${clone.outerHTML} 2003.`,
         a:"since", b:"for", c:"in", answer:"since"
     },
-    { questions: `16.If I ${clone.outerHTML} rich, I’d buy a yacht.`,
+    { questions: `16. If I ${clone.outerHTML} rich, I’d buy a yacht.`,
         a:"were", b:"am", c:"would be", answer:"were"
     },
-    { questions: `17.Rainforests ${clone.outerHTML} down and the trees are used to make paper.`,
+    { questions: `17. Rainforests ${clone.outerHTML} down and the trees are used to make paper.`,
         a:"are cutting", b:"is cut", c:"are cut", answer:"are cut"
     },
-    { questions: `18.He cleaned the carpets after he ${clone.outerHTML} finished his lunch. `,
+    { questions: `18. He cleaned the carpets after he ${clone.outerHTML} finished his lunch. `,
         a:"had", b:"have", c:"has", answer:"had"
     },
-    { questions: `19.I’d rather go out for dinner tonight than ${clone.outerHTML} dinner at home.`,
+    { questions: `19. I’d rather go out for dinner tonight than ${clone.outerHTML} dinner at home.`,
         a:"have", b:"having", c:"to have", answer:"have"
     },
-    { questions: `20.By this time next year, Jane ${clone.outerHTML} Spanish for two years.`,
+    { questions: `20. By this time next year, Jane ${clone.outerHTML} Spanish for two years.`,
         a:"is going to study", b:"will be studying", c:"will have been studying", answer:"will have been studying"
     },
-    { questions: `21.Alice won’t be able to buy that car ${clone.outerHTML} she saves some money.`,
+    { questions: `21. Alice won’t be able to buy that car ${clone.outerHTML} she saves some money.`,
         a:"unless", b:"if", c:"as long as", answer:"unless"
     },
-    { questions: `22.John is soaking wet because he ${clone.outerHTML} in the rain.`,
+    { questions: `22. John is soaking wet because he ${clone.outerHTML} in the rain.`,
         a:"has been walking", b:"will be walking", c:"had been walking", answer:"has been walking"
     },
-    { questions: `23.I wish I ${clone.outerHTML} at Mike; he’s not talking to me now.`,
+    { questions: `23. I wish I ${clone.outerHTML} at Mike; he’s not talking to me now.`,
         a:"hadn't shouted", b:"haven't shouted", c:"didn't shout", answer:"hadn't shouted"
     },
-    { questions: `24.Dogs are very loyal pets. ${clone.outerHTML}, they can guard your property.`,
+    { questions: `24. Dogs are very loyal pets. ${clone.outerHTML}, they can guard your property.`,
         a:"Because", b:"What is more", c:"Even though", answer:"What is more"
     },
-    { questions: `25.Her hair is not ${clone.outerHTML} Catherine’s.`,
+    { questions: `25. Her hair is not ${clone.outerHTML} Catherine’s.`,
         a:"so long", b:"as long as", c:"long as", answer:"as long as"
     },
-    { questions: `26.“I love going to the beach.” “${clone.outerHTML}”`,
+    { questions: `26. “I love going to the beach.” “${clone.outerHTML}”`,
         a:"Neither do I", b:"So do I", c:"Nor do I", answer:"So do I"
     },
-    { questions: `27.By the time the rain stopped, we ${clone.outerHTML} two pots of coffee.`,
+    { questions: `27. By the time the rain stopped, we ${clone.outerHTML} two pots of coffee.`,
         a:"have drunk", b:"had drunk", c:"had been drinking", answer:"had drunk"
     },
-    { questions: `28.He can’t decide who ${clone.outerHTML} to his birthday party.`,
+    { questions: `28. He can’t decide who ${clone.outerHTML} to his birthday party.`,
         a:"to invite", b:"invite", c:"inviting", answer:"to invite"
     },
-    { questions: `29.When I opened the door, there wasn’t ${clone.outerHTML} there.`,
+    { questions: `29. When I opened the door, there wasn’t ${clone.outerHTML} there.`,
         a:"someone", b:"no one", c:"anyone", answer:"anyone"
     },
-    { questions: `30.I live in the city, so I ${clone.outerHTML} busy traffic.`,
+    { questions: `30. I live in the city, so I ${clone.outerHTML} busy traffic.`,
         a:"am used to", b:"get used to", c:"used to", answer:"am used to"
     },
-    { questions: `31.It’s no use ${clone.outerHTML} about the exam results. You’ll know soon enough.`,
+    { questions: `31. It’s no use ${clone.outerHTML} about the exam results. You’ll know soon enough.`,
         a:"worry", b:"to worry", c:"worrying", answer:"worrying"
     },
-    { questions: `32.The taxi will be here in a couple of minutes. We ${clone.outerHTML} get ready to go.`,
+    { questions: `32. The taxi will be here in a couple of minutes. We ${clone.outerHTML} get ready to go.`,
         a:"had better", b:"would better", c:"should better", answer:"had better"
     },
-    { questions: `33.“Dad won’t mind us borrowing the car, will he?” “No, I ${clone.outerHTML}”`,
+    { questions: `33. “Dad won’t mind us borrowing the car, will he?” “No, I ${clone.outerHTML}”`,
         a:"don't suppose it", b:"suppose not", c:"don't suppose", answer:"suppose not"
     },
-    { questions: `34.When I asked what was wrong, ${clone.outerHTML} `,
+    { questions: `34. When I asked what was wrong, ${clone.outerHTML} `,
         a:"I was explained the problem", b:"the problem was explained to me", c:"he explained me the problem", answer:"the problem was explained to me"
     },
-    { questions: `35.	Steven ${clone.outerHTML} the wallet. `,
+    { questions: `35. Steven ${clone.outerHTML} the wallet. `,
         a:"admitted to steal", b:"admitted steal", c:"admitted stealing", answer:"admitted stealing"
     },
-    { questions: `36.We should use ${clone.outerHTML} time we have available to discuss John’s proposal.`,
+    { questions: `36. We should use ${clone.outerHTML} time we have available to discuss John’s proposal.`,
         a:"the little of", b:"the little", c:"little", answer:"the little"
     },
-    { questions: `37.Some experience is ${clone.outerHTML} for the job. `,
+    { questions: `37. Some experience is ${clone.outerHTML} for the job. `,
         a:"really essential", b:"fairly essential", c:"very essential", answer:"really essential"
     },
-    { questions: `38.${clone.outerHTML} in my seventies and rather unfit, I might consider taking up squash.`,
+    { questions: `38. ${clone.outerHTML} in my seventies and rather unfit, I might consider taking up squash.`,
         a:"Were I not", b:"Was I not", c:"If I wasn't", answer:"Were I not"
     },
-    { questions: `39.We were delayed ${clone.outerHTML} an accident.`,
+    { questions: `39. We were delayed ${clone.outerHTML} an accident.`,
         a:"because", b:"because of", c:"of because", answer:"because of"
     },
-    { questions: `40.${clone.outerHTML} they slept soundly.`,
+    { questions: `40. ${clone.outerHTML} they slept soundly.`,
         a:"Hot though the night air was", b:"Hot though was the night air", c:"Hot although the night air was", answer:"Hot though the night air was"
     },
 
